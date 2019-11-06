327833f45ff8e45309b86d487a6f77a4e2f31f86
Dibash Thapa <dibashthapa55@gmail.com>
2019-11-06 21:33:34 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+
+import session
+from flask import *
+from flask_mysqldb import MySQL
+
+ 
+app = Flask(__name__)
+mysql= MySQL(app)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'MyDB'
+
+@app.route('/')
+def hello_world():
+   return render_template("index.html")
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+   if request.method=='GET':
+      return render_template("login.html")
+   
+@app.route('/register',methods=['GET','POST'])
+def register():
+   if request.method=='GET':
+      return render_template("register.html")
+   elif request.method=='POST':
+      details = request.form
+      Name = details['Name']
+      Email = details['Email']
+    
+      Password=details['Password']
+      cur = mysql.connection.cursor()
+      cur.execute("INSERT INTO Users(Name, Email, Password) VALUES(%s, %s, %s)",(Name, Email, Password))
+      mysql.connection.commit()
+      cur.close()
+      return "success"
+
+
+   
+if __name__ == '__main__':
+   app.run(host="localhost",port=5000,debug=True)
