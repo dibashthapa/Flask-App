3c7b591f2f7f7be4568225feae66f1b5dae9bc7d
Dibash Thapa <dibashthapa55@gmail.com>
2019-11-12 17:25:20 +0545

    client side not receiving the name

--- main.py
+++ main.py
@@ -2,68 +2,78 @@
 import session
 from flask import *
 from flask_mysqldb import MySQL
-from flask_socketio import SocketIO,send
+from flask_socketio import SocketIO, send, emit
 
- 
+
 app = Flask(__name__)
-mysql= MySQL(app)
+mysql = MySQL(app)
 socketio = SocketIO(app)
-app.secret_key="dibashthapa"
+app.secret_key = "dibashthapa"
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'MyDB'
 
+users={}
+
 @app.route('/')
 def home():
-   return render_template("index.html")
+    return render_template("index.html")
 
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-   if request.method=='GET':
-      return render_template("login.html")
-   else:
-      details=request.form
-      Email=details['Email']
-      Password=details['Password']
-      cur=mysql.connection.cursor()
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        details = request.form
+        Email = details['Email']
+        Password = details['Password']
+        cur = mysql.connection.cursor()
 
-      query_string = "SELECT * FROM Users WHERE Email = %s AND Password=%s"
-      cur.execute(query_string, (Email,Password))
-      mysql.connection.commit()
-      account=cur.fetchone()
-      if account:
- 
-         session['LoggedIn']=True
-         session['Email']=account[2]
-         session['Name']=account[1]
-         return render_template("index.html",Status=session['LoggedIn'],Name=account[1])
-     
-         
-      else:
-         session['LoggedIn']=False
-         return render_template("login.html",status=session['LoggedIn'])
-      
-@app.route('/register',methods=['GET','POST'])
+        query_string = "SELECT * FROM Users WHERE Email = %s AND Password=%s"
+        cur.execute(query_string, (Email, Password))
+        mysql.connection.commit()
+        account = cur.fetchone()
+        if account:
+
+            session['LoggedIn'] = True
+            session['Email'] = account[2]
+            session['Name'] = account[1]
+            return render_template("index.html", Status=session['LoggedIn'], Name=account[1])
+
+        else:
+            session['LoggedIn'] = False
+            return render_template("login.html", status=session['LoggedIn'])
+
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-   if request.method=='GET':
-      return render_template("register.html")
-   elif request.method=='POST':
-      details = request.form
-      Name = details['Name']
-      Email = details['Email']
-    
-      Password=details['Password']
-      cur = mysql.connection.cursor()
-      cur.execute("INSERT INTO Users(Name, Email, Password) VALUES(%s, %s, %s)",(Name, Email, Password))
-      mysql.connection.commit()
-      cur.close()
-      return "success"
-@socketio.on('message')
-def handleMessage(msg):
-	print('Message: ' + msg)
-	send(msg, broadcast=True)
+    if request.method == 'GET':
+        return render_template("register.html")
+    elif request.method == 'POST':
+        details = request.form
+        Name = details['Name']
+        Email = details['Email']
+
+        Password = details['Password']
+        cur = mysql.connection.cursor()
+        cur.execute(
+            "INSERT INTO Users(Name, Email, Password) VALUES(%s, %s, %s)", (Name, Email, Password))
+        mysql.connection.commit()
+        cur.close()
+        return redirect("/login")
+
+
+@socketio.on('message from user',namespace='/message')
+def handleMessage(message,name):
+   print('Message:' + message)
+   print(name,'has sent a message')
+
+   emit('from flask',message,name,broadcast=True)
+
+
+
 
 if __name__ == '__main__':
-	socketio.run(app,debug=True)
+    socketio.run(app, debug=True)
